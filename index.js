aabc2ba7db91236fa16f0a6a05f57273d9206a79
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-04 14:23:58 -0400

    Fixing a bind issue

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const path = require("path"),
 function factory (config) {
 	let obj = new ETag(config);
 
-	obj.create = obj.create.bind(obj);
-	obj.middleware = obj.middleware.bind(obj);
+	obj.middleware = obj.middleware();
 
 	return obj;
 }
