e8e024f352fd1076f9ba20e1b942a94552dae9a3
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-05 23:46:35 -0400

    Fixing cache life cycle, adding tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const http = require("http"),
 	path = require("path"),
 	random = Math.floor(Math.random()*9)+1,
 	mmh3 = require("murmurhash3js").x86.hash32,
+	etagValue = "\"" + mmh3("Hello World!", random) + "\"",
 	cacheSize = 1000;
 
 let router = require("woodland")({defaultHeaders: {"Cache-Control": "no-cache"}, cacheSize: cacheSize, defaultHost: "localhost", hosts: ["localhost", "noresponse"]}),
@@ -14,22 +15,12 @@ function request () {
 	return hippie().base("http://localhost:8001");
 }
 
-router.use(etag.middleware).blacklist(etag.middleware);
+router.use(etag.middleware, "all").blacklist(etag.middleware);
 
 router.use("/", function hello (req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain", "ETag": etag.create("Hello World!")});
 	res.end("Hello World!");
 });
-
-router.use("/echo/:echo", (req, res) => {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end(req.params.echo);
-});
-
-router.use("/nothere.html", (req, res) => {
-	res.writeHead(204);
-	res.end("");
-}, "GET", "noresponse");
 
 http.createServer(router.route).listen(8001);
 
@@ -41,7 +32,7 @@ describe("Valid ETag", function () {
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "no-cache")
 			.expectHeader("Content-Type", "text/plain")
-			.expectHeader("ETag", "\"" + mmh3("Hello World!", random) + "\"")
+			.expectHeader("ETag", etagValue)
 			.expectBody(/^Hello World!$/)
 			.end(function (err) {
 				if (err) throw err;
@@ -56,7 +47,37 @@ describe("Valid ETag", function () {
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "no-cache")
 			.expectHeader("Content-Type", "text/plain")
-			.expectHeader("ETag", "\"" + mmh3("Hello World!", random) + "\"")
+			.expectHeader("ETag", etagValue)
+			.expectBody(/^$/)
+			.end(function (err) {
+				if (err) throw err;
+				done();
+			});
+	});
+
+	it("GET / (304 / empty)", function (done) {
+		request()
+			.get("/")
+			.header("If-None-Match", etagValue)
+			.expectStatus(304)
+			.expectHeader("Age", /\d+/)
+			.expectHeader("Content-Length", undefined)
+			.expectHeader("ETag", etagValue)
+			.expectBody(/^$/)
+			.end(function (err) {
+				if (err) throw err;
+				done();
+			});
+	});
+
+	it("GET / (304 / empty & validation)", function (done) {
+		request()
+			.get("/")
+			.header("If-None-Match", etagValue)
+			.expectStatus(304)
+			.expectHeader("Age", /\d+/)
+			.expectHeader("Content-Length", undefined)
+			.expectHeader("ETag", etagValue)
 			.expectBody(/^$/)
 			.end(function (err) {
 				if (err) throw err;
