9f4fa1ad80d9756faa2c84602726ae419e77c9eb
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-04-12 06:59:26 -0400

    Changing test frameworks, updating LRU cache

--- test/test.js
+++ test/test.js
@@ -7,12 +7,8 @@ const http = require("http"),
 	etagValue = "\"" + mmh3("Hello World!", random) + "\"",
 	cacheSize = 1000,
 	router = require("woodland")({defaultHeaders: {"Cache-Control": "public"}, cacheSize: cacheSize, defaultHost: "localhost", hosts: ["localhost", "noresponse"]}),
-	hippie = require("hippie"),
+	tinyhttptest = require("tiny-httptest"),
 	etag = require(path.join(__dirname, "..", "index.js"))({cacheSize: cacheSize, seed: random});
-
-function request () {
-	return hippie().base("http://localhost:8001");
-}
 
 router.use(etag.middleware, "all").blacklist(etag.middleware);
 
@@ -29,93 +25,67 @@ router.use("/no-cache", function hello (req, res) {
 http.createServer(router.route).listen(8001);
 
 describe("Valid ETag", function () {
-	it("GET / (200 / 'Success')", function (done) {
-		request()
-			.get("/")
+	it("GET / (200 / 'Success')", function () {
+		return tinyhttptest({url: "http://localhost:8001/"})
 			.expectStatus(200)
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "public")
 			.expectHeader("Content-Type", "text/plain")
 			.expectHeader("ETag", etagValue)
 			.expectBody(/^Hello World!$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 
-	it("HEAD / (200 / 'Success')", function (done) {
-		request()
-			.head("/")
+	it("HEAD / (200 / 'Success')", function () {
+		return tinyhttptest({url: "http://localhost:8001/", method: "head"})
 			.expectStatus(200)
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "public")
 			.expectHeader("Content-Type", "text/plain")
 			.expectHeader("ETag", etagValue)
 			.expectBody(/^$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 
-	it("GET / (304 / empty)", function (done) {
-		request()
-			.get("/")
-			.header("If-None-Match", etagValue)
+	it("GET / (304 / empty)", function () {
+		return tinyhttptest({url: "http://localhost:8001/", headers:{"If-None-Match": etagValue}})
 			.expectStatus(304)
 			.expectHeader("Age", /\d+/)
 			.expectHeader("Content-Length", undefined)
 			.expectHeader("ETag", etagValue)
 			.expectBody(/^$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 
-	it("GET / (304 / empty & validation)", function (done) {
-		request()
-			.get("/")
-			.header("If-None-Match", etagValue)
+	it("GET / (304 / empty & validation)", function () {
+		return tinyhttptest({url: "http://localhost:8001/", headers:{"If-None-Match": etagValue}})
 			.expectStatus(304)
 			.expectHeader("Age", /\d+/)
 			.expectHeader("Content-Length", undefined)
 			.expectHeader("ETag", etagValue)
 			.expectBody(/^$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 
-	it("GET /no-cache (200 / 'Success' / No Etag)", function (done) {
-		request()
-			.get("/no-cache")
+	it("GET /no-cache (200 / 'Success' / No Etag)", function () {
+		return tinyhttptest({url: "http://localhost:8001/no-cache"})
 			.expectStatus(200)
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "no-cache")
 			.expectHeader("Content-Type", "text/plain")
 			.expectHeader("ETag", undefined)
 			.expectBody(/^Hello World!$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 
-	it("HEAD /no-cache (200 / 'Success' / No Etag)", function (done) {
-		request()
-			.head("/no-cache")
+	it("HEAD /no-cache (200 / 'Success' / No Etag)", function () {
+		return tinyhttptest({url: "http://localhost:8001/no-cache", method: "head"})
 			.expectStatus(200)
 			.expectHeader("Allow", "GET, HEAD, OPTIONS")
 			.expectHeader("Cache-Control", "no-cache")
 			.expectHeader("Content-Type", "text/plain")
 			.expectHeader("ETag", undefined)
 			.expectBody(/^$/)
-			.end(function (err) {
-				if (err) throw err;
-				done();
-			});
+			.end();
 	});
 });
